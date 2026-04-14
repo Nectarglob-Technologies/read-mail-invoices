4afe6febb3cbb442933d3c00020eff7e2ec59f65
Nilesh Winerkar <nilesh.winerkar@nectarglob.com>
2026-04-14 08:13:04 +0530

    changed start.sh file added stremlit start

--- start.sh
+++ start.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # Start backend
-uvicorn backend.main:app --host 0.0.0.0 --port 8000 &
+#uvicorn backend.main:app --host 0.0.0.0 --port 8000 &
 
 # Start streamlit
-streamlit run frontend_streamlit.py --server.port $PORT --server.address 0.0.0.0
+#streamlit run frontend_streamlit.py --server.port $PORT --server.address 0.0.0.0
+
+#!/bin/bash
+streamlit run streamlit_app.py --server.port 8000 --server.address 0.0.0.0
